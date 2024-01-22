1863290a0b9923f52c10fef582ffa171252ed7f8
anarka312 <natalie.talas@gmail.com>
2024-01-22 17:45:29 +0600

    live server npm

---
+++ 05-react-jsx-green-button/script.js
@@ -0,0 +1,18 @@
+
+const App = () => {
+    let buttonText = 'Click me';
+
+    const onButtonClick = () => {
+      buttonText = 'Hello from React ';
+    };
+
+    return (
+      <div className="app">
+        <button onClick={onButtonClick}>{buttonText}</button>
+      </div>
+    );
+
+}
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
