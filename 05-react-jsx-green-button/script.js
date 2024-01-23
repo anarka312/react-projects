031cc76c1dffde8a541aaead111e8e5916baa584
anarka312 <natalie.talas@gmail.com>
2024-01-23 23:03:53 +0600

    script js

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,18 +1,16 @@
-
 const App = () => {
-    let buttonText = 'Click me';
+  const [buttonText, setButtonTextReact] = useState('Click me');
 
-    const onButtonClick = () => {
-      buttonText = 'Hello from React ';
-    };
+  const onButtonClick = () => {
+    buttonText = 'Hello from React ';
+  };
 
-    return (
-      <div className="app">
-        <button onClick={onButtonClick}>{buttonText}</button>
-      </div>
-    );
-
-}
+  return (
+    <div className="app">
+      <button onClick={onButtonClick}>{buttonText}</button>
+    </div>
+  );
+};
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
