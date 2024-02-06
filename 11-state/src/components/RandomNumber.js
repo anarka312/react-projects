dbb16bf3874c12b701fa46000fef8544a0bdc0b4
anarka312 <natalie.talas@gmail.com>
2024-02-06 23:53:45 +0600

    random num

--- 11-state/src/components/RandomNumber.js
+++ 11-state/src/components/RandomNumber.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
+function generateRandomNum() {
+  return Math.floor(Math.random() * 100);
+}
+
 function RandomNumber() {
+  const [randomNum, setRandomNum] = useState(generateRandomNum());
+  const changeRandomNum = () => {
+    setRandomNum(generateRandomNum());
+  };
+
   return (
     <div>
-      <h1>100</h1>
-      <button>Generate new number</button>
+      <h1>{randomNum}</h1>
+      <button onClick={changeRandomNum}>Generate new number</button>
     </div>
   );
 }
