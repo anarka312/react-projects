88eb44baa8fa83ffdd3b9fcf48e542a9a8e07ddd
anarka312 <natalie.talas@gmail.com>
2024-02-12 09:51:44 +0600

    map array of object

--- 14-map-array-of-object/src/App.js
+++ 14-map-array-of-object/src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import persons from './data/persons';
-import Person from './components/Person';
+
+import Persons from './components/Persons';
+
 
 function App() {
   return (
     <div className="App">
-      {persons.map((person) => {
-        const { id, firstName, lastName, email, img } = person;
-        return (
-          <Person
-            id={id}
-            firstName={firstName}
-            lastName={lastName}
-            email={email}
-            img={img}
-          />
-        );
-      })}
+      <Persons />
     </div>
   );
 }
