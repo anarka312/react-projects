3d58d064a2a51edb07e9ede138532dc3cf2a1e51
anarka312 <natalie.talas@gmail.com>
2024-02-27 23:49:06 +0600

    loading

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -5,14 +5,18 @@ import Post from './Post';
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((res) => res.json())
       .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message));
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false))
   }, []);
 
-
+  if(isLoading) {
+    return <h1>Loading...</h1>
+  }
   if (error) {
     return <h1>Error:{error}</h1>
   }
