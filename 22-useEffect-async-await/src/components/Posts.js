53d957521ec124a5a929d05cea099e88756b9f2a
anarka312 <natalie.talas@gmail.com>
2024-03-16 18:08:38 +0600

    async await

--- 22-useEffect-async-await/src/components/Posts.js
+++ 22-useEffect-async-await/src/components/Posts.js
@@ -7,13 +7,27 @@ function Posts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
+
+  useEffect( () => {
+    ;(async function () {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    })
   }, []);
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((res) => res.json())
+  //     .then((posts) => setPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
 
   if (error) {
     return <h1>Error:{error}</h1>;
